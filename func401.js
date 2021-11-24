feac374da7bb2b78372833969876167f0e5cbac2
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-24 13:29:02 +0700

    initial commit

---
+++ func401.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+    //console.log(a);
+    return a + b;
+}
+function sub(c, d) {
+    return c - d;
+}
+console.log(sub(10, 5));
+console.log(add(5, 10));
---
+++ func401.ts
@@ -0,0 +1,11 @@
+function add(a: number, b: number): number {
+    //console.log(a);
+    return a + b;
+}
+
+function sub(c: number, d: number): number {
+    return c - d;
+}
+console.log(sub(10,5));
+console.log(add(5, 10));
+
